ed1f86440709436f9053f78b2e221d2b4dfbf510
expher <expher@expher.savage3d>
2021-04-21 20:10:42 -0700

    api guarda archivos

---
+++ src/main/java/com/file/upload/UploadApplication.java
@@ -0,0 +1,38 @@
+package com.file.upload;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@SpringBootApplication
+@RestController
+public class UploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadApplication.class, args);
+	}
+
+	@RequestMapping(value="/upload", method=RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+		File convertFile = new File("/home/expher/"+file.getOriginalFilename());
+		convertFile.createNewFile();
+		FileOutputStream fout = new FileOutputStream(convertFile);
+		fout.write(file.getBytes());
+		fout.close();
+		return new ResponseEntity<>("File is uploaded successfully", HttpStatus.OK);
+	}
+
+
+
+}
---
+++ src/main/java/com/file/upload/rest.java
@@ -0,0 +1,29 @@
+package com.file.upload;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Controller
+@RequestMapping("/fileupload")
+public class rest {
+
+    @RequestMapping(value = "/file", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody String myService(@RequestParam("file") MultipartFile file,
+                                          @RequestParam("id") String id) throws Exception {
+
+        if (!file.isEmpty()) {
+
+            //your logic
+        }
+        return "some json";
+
+    }
+}
